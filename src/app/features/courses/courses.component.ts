d5264a69c6913228c6cb51890270f26311b94c16
Jolly Chua <jollygrace.c.3@gmail.com>
2024-11-24 05:12:18 +0800

    Updated UI of fetch all books and integrated api

--- src/app/features/courses/courses.component.ts
+++ src/app/features/courses/courses.component.ts
@@ -1,14 +1,33 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TopNavComponent } from '../../shared/top-nav/top-nav.component';
-
+import { CourseService } from '../../services/course.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [TopNavComponent],
+  imports: [CommonModule, TopNavComponent],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
+  courses: { bookid: number; bookname: string; author: string; price: number }[] = [];
+  constructor(private courseService: CourseService) {}
 
+  ngOnInit(): void {
+    this.fetchCourses();
+  }
+
+
+  fetchCourses(): void {
+    this.courseService.showCourses().subscribe(
+      (response) => {
+        console.log('All courses:', response);
+        this.courses = response; // Assuming the API returns an array of courses
+      },
+      (error: any) => {
+        console.error('Error fetching all courses:', error);
+      }
+    );
+  }
 }
