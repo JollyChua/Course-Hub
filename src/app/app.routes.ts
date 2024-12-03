4af20b6915da49f861bbc9fcde1c5edcf13aef5f
ibanvoltair <ibanvoltair@gmail.com>
2024-12-04 05:15:25 +0800

    modify navbar to include Books

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './features/registration/home.component';
 import { CoursesComponent } from './features/courses/courses.component';
 import { AddCourseComponent } from './features/add-course/add-course.component';
 import { FetchAllUsersComponent } from './features/fetch-all-users/fetch-all-users.component';
+import { FetchAllBooksComponent } from './features/fetch-all-books/fetch-all-books.component';
 import { RouterModule, Routes } from '@angular/router'; 
 
 export const routes: Routes = [
@@ -10,6 +11,7 @@ export const routes: Routes = [
   { path: 'courses', component: CoursesComponent },  // Your target route
   { path: 'add-course', component: AddCourseComponent },
   { path: 'fetch-all-users', component: FetchAllUsersComponent },
+  { path: 'fetch-all-books', component: FetchAllBooksComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' }  // Wildcard route for 404
 ];
